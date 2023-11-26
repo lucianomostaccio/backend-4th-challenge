0c0d50d6d0655db3338bf2ce7b7f251fa79f2443
Luciano Mostaccio <lucianomostaccio@gmail.com>
2023-11-25 22:31:49 -0300

    minor bugfix for import __dirname, as it was migrated to utils.js

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,11 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import __dirname from "../src/utils.js";
 const filePath = path.join(__dirname, "../products.json");
 
 class ProductManager {
